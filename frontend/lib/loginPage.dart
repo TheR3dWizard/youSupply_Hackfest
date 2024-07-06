583b47294990f7bcdcb1813304dd7a89ca8893af
Sanjithaa26 <sanjitha20041234@gmail.com>
2024-07-06 16:29:45 +0530

    created some files

---
+++ frontend/lib/loginPage.dart
@@ -0,0 +1,6 @@
+// Login page should allow users to enter their name and password
+// and also mention whether they are general users(requesters/providers)
+// or delivery agents
+//
+
+import 'package:flutter/material.dart';
