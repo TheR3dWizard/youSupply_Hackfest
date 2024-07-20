93313d87028da80fe871bc5ee7a329504af976ba
TheRedWizard <sreeraghavan2016@gmail.com>
2024-07-20 19:05:09 +0530

    removed dotenv

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -10,7 +10,6 @@ import 'package:frontend/generalUsers/homePageGU.dart';
 import 'delAgents/history.dart';
 import 'settings.dart';
 import 'profile.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 //import 'frontend/lib/delAgents/available.dart';
 
 // void main() {
@@ -21,7 +20,7 @@ Future main() async {
   // To load the .env file contents into dotenv.
   // NOTE: fileName defaults to .env and can be omitted in this case.
   // Ensure that the filename corresponds to the path in step 1 and 2.
-  await dotenv.load(fileName: ".env");
+  
   //...runapp
   runApp(const MyApp());
 }
