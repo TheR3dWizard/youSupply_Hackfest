c0e2a51e194ae85dba2eaaabce5f94308274873c
TheRedWizard <sreeraghavan2016@gmail.com>
2024-07-24 22:39:31 +0530

    added plotting in nonincremental

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -42,6 +42,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: loggedIn ? '/homegu' : '/login',
       onGenerateRoute: (settings) {
         if (settings.name == '/claimed') {
