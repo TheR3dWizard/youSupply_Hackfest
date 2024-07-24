35c68d7537f0ac31e7f90b9a18137090d9826520
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-07-24 08:48:27 +0530

    path fix and tried vector database integration

--- AlgorithmLogic/server.py
+++ AlgorithmLogic/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from algorithm import System, Cluster, Node
-from flask import request
-from services import DatabaseObject
+from algorithm import System, Cluster, Node, PathComputationObject
+from flask import request,jsonify
+from services import DatabaseObject, VectorDatabaseObject
 import json
 
 def read_json_file(file_path):
@@ -9,8 +9,13 @@ def read_json_file(file_path):
         data = json.load(file)
     return data
 
+computepathobject = PathComputationObject()
 centralsystemobject = System(distancelimit=5)
 databaseobject = DatabaseObject()
+vectordatabase = VectorDatabaseObject()
+
+centralsystemobject.addrequest(Node(x_pos=1, y_pos=1, item="Flashlight", quantity=1))
+centralsystemobject.addrequest(Node(x_pos=1, y_pos=1, item="Flashlight", quantity=-1))
 
 app = Flask(__name__)
 
@@ -31,7 +36,9 @@ def addrequest():
         quantity=body["quantity"],
     )
     updatedcluster = centralsystemobject.addrequest(newnode)
-
+    
+    computepathobject.setSystem(centralsystemobject)
+    
     databaseobject.insertrequest(
         requestid=newnode.identifier,
         resourceid=body["itemid"],
@@ -42,13 +49,39 @@ def addrequest():
         newlon=updatedcluster.centerxpos,
     )
 
+    recomputedpaths = computepathobject.getPaths()
+    
+    vectordatabase.removepathobjects()
+    for path in recomputedpaths:
+        if not path:
+            continue
+        print(path)
+        vectordatabase.insertpathobject(
+            pathobjectid=path.identifier,
+            coordinates=[path.xposition, path.yposition], 
+        )
+    
     return centralsystemobject.stats()
 
 @app.route("/sample/paths", methods=["POST"])
 def getpaths():
     body = request.get_json()
     return read_json_file("samplepaths.json")
-    
+
+from flask import jsonify, request
+
+@app.route("/get/paths", methods=["POST"])
+def getactualpaths():
+    body = request.get_json()
+    result = vectordatabase.getnearestneighbors([body["xposition"], body["yposition"]])
+
+    # Convert the result to a JSON-serializable format
+    json_serializable_result = {
+        "neighbors": [neighbor.to_dict() for neighbor in result]
+    }
+
+    return jsonify(json_serializable_result)
+
 
 @app.route("/get/stats")
 def stats():
