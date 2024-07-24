7f5ab58dec946d7253105b8ed95d71e2fc2c2ce8
Pramo20 <pramodheepa@gmail.com>
2024-07-24 19:29:20 +0530

    podfile and delpage

--- frontend/lib/delAgents/claimed.dart
+++ frontend/lib/delAgents/claimed.dart
@@ -16,12 +16,14 @@ class ClaimedRoutes extends StatefulWidget {
 class _ClaimedRoutesState extends State<ClaimedRoutes> {
   late Future<List<Tuple>> _pathTuplesFuture;
   late List<bool> _completedStatus;
-  bool _acceptPressed = false;
 
   @override
   void initState() {
     super.initState();
-    _pathTuplesFuture = loadPathsTuple(widget.pathIndex);
+    _pathTuplesFuture = loadPathsTuple(widget.pathIndex).then((pathTuples) {
+      _completedStatus = List<bool>.filled(pathTuples.length, false);
+      return pathTuples;
+    });
   }
 
   @override
@@ -50,152 +52,113 @@ class _ClaimedRoutesState extends State<ClaimedRoutes> {
             return Center(child: Text('No data available'));
           } else {
             List<Tuple> pathTuples = snapshot.data!;
-            _completedStatus = List<bool>.filled(pathTuples.length, false);
-            print('Path Tuples: $pathTuples'); // Debug print
+
             return Column(
               children: [
                 Expanded(
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: ListView.builder(
-                            itemCount: pathTuples.length,
-                            itemBuilder: (context, index) {
-                              Tuple currentTuple = pathTuples[index];
-                              bool isCompleted = _completedStatus[index];
-                              String nextLocation =
-                                  index < pathTuples.length - 1
-                                      ? pathTuples[index + 1].startLoc
-                                      : 'End';
+                    child: ListView.builder(
+                      itemCount: pathTuples.length,
+                      itemBuilder: (context, index) {
+                        Tuple currentTuple = pathTuples[index];
+                        bool isCompleted = _completedStatus[index];
+                        String nextLocation = index < pathTuples.length - 1
+                            ? pathTuples[index + 1].startLoc
+                            : 'End';
 
-                              return Container(
-                                padding: const EdgeInsets.all(10.0),
-                                margin: const EdgeInsets.only(bottom: 16.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.grey[850],
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Expanded(
-                                          child: Text(
-                                            currentTuple.startLoc,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 18,
-                                              color: Colors.white70,
-                                            ),
-                                            overflow: TextOverflow.ellipsis,
-                                          ),
-                                        ),
-                                        Icon(
-                                          Icons.arrow_forward,
-                                          color: Colors.white70,
-                                          size: 20,
-                                        ),
-                                        Expanded(
-                                          child: Text(
-                                            nextLocation,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 18,
-                                              color: Colors.white70,
-                                            ),
-                                            overflow: TextOverflow.ellipsis,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(height: 10),
-                                    Text(
-                                      currentTuple.resources,
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        color: Colors.white60,
-                                      ),
-                                    ),
-                                    SizedBox(height: 5),
-                                    if (_acceptPressed)
-                                      Align(
-                                        alignment: Alignment.bottomRight,
-                                        child: isCompleted
-                                            ? Container(
-                                                padding:
-                                                    const EdgeInsets.symmetric(
-                                                        horizontal: 10,
-                                                        vertical: 5),
-                                                child: Text(
-                                                  'Completed',
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 16,
-                                                    fontWeight: FontWeight.w300,
-                                                  ),
-                                                ),
-                                              )
-                                            : OutlinedButton(
-                                                onPressed: () {
-                                                  setState(() {
-                                                    _completedStatus[index] =
-                                                        true;
-                                                  });
-                                                },
-                                                style: OutlinedButton.styleFrom(
-                                                  backgroundColor:
-                                                      Color.fromARGB(
-                                                          255, 0, 255, 255),
-                                                  padding: EdgeInsets.symmetric(
-                                                      vertical: 10,
-                                                      horizontal: 15),
-                                                ),
-                                                child: Text(
-                                                  'Mark as Completed',
-                                                  style: TextStyle(
-                                                    color: Colors.black87,
-                                                  ),
-                                                ),
-                                              ),
-                                      ),
-                                  ],
-                                ),
-                              );
-                            },
+                        return Container(
+                          padding: const EdgeInsets.all(10.0),
+                          margin: const EdgeInsets.only(bottom: 16.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[850],
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                        ),
-                      ],
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      currentTuple.startLoc,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                        color: Colors.white70,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.white70,
+                                    size: 20,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      nextLocation,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                        color: Colors.white70,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                currentTuple.resources,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white60,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: isCompleted
+                                    ? Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10, vertical: 5),
+                                        child: Text(
+                                          'Completed',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w300,
+                                          ),
+                                        ),
+                                      )
+                                    : ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            _completedStatus[index] = true;
+                                          });
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              Color.fromARGB(255, 0, 255, 255),
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 10, horizontal: 15),
+                                        ),
+                                        child: Text(
+                                          'Mark as Completed',
+                                          style: TextStyle(
+                                            color: Colors.black87,
+                                          ),
+                                        ),
+                                      ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
                   ),
                 ),
-                if (!_acceptPressed)
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _acceptPressed = true;
-                        });
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        padding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                        shape: ContinuousRectangleBorder(),
-                        minimumSize: Size(450, 25),
-                      ),
-                      child: Text(
-                        'Accept',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                    ),
-                  ),
               ],
             );
           }
