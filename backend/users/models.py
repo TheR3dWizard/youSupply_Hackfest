865c1e6c2279a5f3094bff25b1e6070c043035f9
TheR3dWizard <sreeraghavan2016@gmail.com>
2024-09-14 20:03:49 +0530

    created User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    Username = models.CharField(max_length=50)
+    Location = models.PointField()
+    Password = models.CharField(max_length=50)
