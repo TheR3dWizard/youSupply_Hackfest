f4e74c959da29c2ad7bc36cd717df28668a7e865
TheRedWizard <sreeraghavan2016@gmail.com>
2024-09-17 16:19:39 +0530

    removed cd from build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-cd backend
+
 
 # Convert static asset files
 python manage.py collectstatic --no-input
