0e8123cccc98926ba72607ea95b74727d94ddfce
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-09-28 19:35:14 +0530

    black formatting changes

--- AlgorithmLogic/server.py
+++ AlgorithmLogic/server.py
@@ -7,20 +7,27 @@ import uuid
 from pprint import pprint
 from typing import List
 
+
 def read_json_file(file_path):
     with open(file_path, "r") as file:
         data = json.load(file)
     return data
+
 
 computepathobject = PathComputationObject()
 centralsystemobject = System(distancelimit=5)
 databaseobject = DatabaseObject()
 chromadbagent = ChromaDBAgent()
 masterpathlist: List[Path] = list()
-centralsystemobject.addrequest(Node(x_pos=40.646984, y_pos=-73.789450, item="Flashlight", quantity=1))
-centralsystemobject.addrequest(Node(x_pos=42.646984, y_pos=-73.789450, item="Flashlight", quantity=-1))
+centralsystemobject.addrequest(
+    Node(x_pos=40.646984, y_pos=-73.789450, item="Flashlight", quantity=1)
+)
+centralsystemobject.addrequest(
+    Node(x_pos=42.646984, y_pos=-73.789450, item="Flashlight", quantity=-1)
+)
 gmagent = GoogleAPI()
 app = Flask(__name__)
+
 
 def getNodeObject(self, nodeid: str):
     result = databaseobject.getNode(nodeid)
@@ -42,11 +49,12 @@ def getNodeObject(self, nodeid: str):
 def home():
     return "this API will be used to natively interact with the algorithm logic"
 
-#WORKS
+
+# WORKS
 @app.route("/add/node", methods=["POST"])
 def addrequest():
     body = request.get_json()
-    '''
+    """
     new body format
     {
         "nodelist": [
@@ -66,8 +74,7 @@ def addrequest():
             }
         ]
     }
-    '''
-
+    """
 
     for node in body["nodelist"]:
         randomid = str(uuid.uuid4())
@@ -78,8 +85,8 @@ def addrequest():
             item=node["itemid"],
             quantity=node["quantity"],
         )
-        
-        chromadbagent.insertnodeobject(randomid,node_obj)
+
+        chromadbagent.insertnodeobject(randomid, node_obj)
         resource_id = databaseobject.getresourceid(node["itemid"])
         quantity = node["quantity"]
         username = node["username"]
@@ -93,9 +100,9 @@ def addrequest():
             username=username,
             latitude=latitude,
             longitude=longitude,
-            action=action
+            action=action,
         )
-        
+
         centralsystemobject.addrequest(node_obj)
 
     return "Worked"
@@ -103,41 +110,43 @@ def addrequest():
 
 @app.route("/path/get", methods=["GET"])
 def serveassortment():
-    example='''
+    example = """
         {
             username :"john_doe"
         }
-    '''
+    """
 
     body = request.get_json()
-    userid = databaseobject.getuserid(body['username'])
-    xposition,yposition = databaseobject.getlocfordelagent(userid)
-    
-    nodeidlist = chromadbagent.getnearestneighbors([float(xposition), float(yposition)], kval=100)
-    
+    userid = databaseobject.getuserid(body["username"])
+    xposition, yposition = databaseobject.getlocfordelagent(userid)
+
+    nodeidlist = chromadbagent.getnearestneighbors(
+        [float(xposition), float(yposition)], kval=100
+    )
+
     masternodeslist = centralsystemobject.getnodes()
     assortednodeslist: List[Node] = []
-    print("Masternodes ",masternodeslist)
-    print("Nodeidlist ",nodeidlist)
-    
+    print("Masternodes ", masternodeslist)
+    print("Nodeidlist ", nodeidlist)
+
     for node in masternodeslist:
         print(f"Checking now {node.identifier}")
         if node and node.identifier in nodeidlist[0]:
             assortednodeslist.append(node)
-    print("Assorted ",assortednodeslist)
+    print("Assorted ", assortednodeslist)
     # TODO AKASH: check if distancelimit = inf is fine
     # pathsystem = System(distancelimit=float('inf'))
-    
+
     # for node in assortednodeslist:
     #     pathsystem.addrequest(node)
-    # TODO AKASH: check if we can do setSystem mutiple times on the 
+    # TODO AKASH: check if we can do setSystem mutiple times on the
     # same object with every time a different system
     computepathobject.setCluster(assortednodeslist)
-    
+
     print("System set")
     paths = computepathobject.getPathsFromCluster()
-    
-    exampleoutput= '''
+
+    exampleoutput = """
     [{
         pathinfo:uuid.uuid4()
         nodes:[
@@ -149,53 +158,54 @@ def serveassortment():
         ]
     },
     ]
-    '''
+    """
     print(paths)
     # TODO AKASH: formart the paths properly
     formatted_paths = []
     for path in paths:
         formatted_path = {
             "pathinfo": str(uuid.uuid4()),
-            "nodes": [{"nodeid": node.identifier} for node in path.path]
+            "nodes": [{"nodeid": node.identifier} for node in path.path],
         }
         formatted_paths.append(formatted_path)
 
-    output = {
-        "numpaths": len(formatted_paths),
-        "paths": {}
-    }
+    output = {"numpaths": len(formatted_paths), "paths": {}}
 
     for i, path in enumerate(formatted_paths):
         path_details = []
         for node in path["nodes"]:
             node_obj = databaseobject.getNode(node["nodeid"])[0]
             # [('4352412b-bfb2-4988-8960-1ba8219f2787', '2', 'cluster1', 10, 'john_doe', Decimal('10.99141700'), Decimal('77.00431300'), 'FREE', 'PICKUP')]
-            
-            path_details.append({
-                "id":node["nodeid"],
-                "itemtype": node_obj[1].rstrip(),
-                "quantity": node_obj[3],
-                "latitude": float(node_obj[5]),
-                "longitude": float(node_obj[6]),
-                "inwords": gmagent.geocodecoordinatestoaddress([float(node_obj[5]), float(node_obj[6])])
-            })
+
+            path_details.append(
+                {
+                    "id": node["nodeid"],
+                    "itemtype": node_obj[1].rstrip(),
+                    "quantity": node_obj[3],
+                    "latitude": float(node_obj[5]),
+                    "longitude": float(node_obj[6]),
+                    "inwords": gmagent.geocodecoordinatestoaddress(
+                        [float(node_obj[5]), float(node_obj[6])]
+                    ),
+                }
+            )
         output["paths"][str(i)] = {
             "path_details": path_details,
-            "nodeids": [node["nodeid"] for node in path["nodes"]]
+            "nodeids": [node["nodeid"] for node in path["nodes"]],
         }
 
     return json.dumps(output, indent=4)
-  
+
 
 @app.route("/path/lookup", methods=["GET"])
 def getpath():
-    exampleinput='''
+    exampleinput = """
     {
         userid:
     }
-    '''
+    """
     body = request.get_json()
-    route_id = databaseobject.getrouteid(body['userid'])
+    route_id = databaseobject.getrouteid(body["userid"])
     steps = databaseobject.getsteps(route_id)
     output = {}
     output["nodes"] = []
@@ -207,11 +217,11 @@ def getpath():
 
     return output
 
+
 @app.route("/path/accept", methods=["POST"])
 def acceptpath():
-
     body = request.get_json()
-    '''
+    """
     {
         "userid": "JohnDoe",
         nodes: [
@@ -220,37 +230,41 @@ def acceptpath():
             nodeid3
         ]    
     }
-    '''
+    """
     userid = body["userid"]
     nodeids = body["nodes"]
     routeid = str(uuid.uuid4())
-    databaseobject.create_route_assignment(userid=userid,routeid=routeid)
+    databaseobject.create_route_assignment(userid=userid, routeid=routeid)
 
     step = 0
     for nodeid in nodeids:
-        databaseobject.create_route_step(route_id=routeid,node_id=nodeid,step_id=step)
+        databaseobject.create_route_step(route_id=routeid, node_id=nodeid, step_id=step)
         step += 1
     return "Worked"
 
-@app.route("/path/markstep",methods=["POST"])
+
+@app.route("/path/markstep", methods=["POST"])
 def markstep():
     body = request.get_json()
-    '''
+    """
     {
         "userid": "JohnDoe",
     }
-    '''
+    """
     step = databaseobject.markstep(body["userid"])
     return step
+
 
 @app.route("/sample/paths", methods=["GET", "POST"])
 def getpaths():
     fd = open("./samplepaths.json")
     return fd.read()
 
+
 @app.route("/get/stats")
 def stats():
     return centralsystemobject.stats()
+
 
 @app.route("/get/db/stats", methods=["GET"])
 def dbstats():
@@ -261,41 +275,51 @@ def dbstats():
 def allchroma():
     return chromadbagent.getallvectors()
 
-@app.route('/config/database/get/users')
+
+@app.route("/config/database/get/users")
 def getusers():
     return databaseobject.getusers()
 
-@app.route('/config/database/get/deliveryvolunteers')
+
+@app.route("/config/database/get/deliveryvolunteers")
 def getdeliveryvolunteers():
     return databaseobject.getdeliveryvolunteers()
 
-@app.route('/config/database/get/resources')
+
+@app.route("/config/database/get/resources")
 def getresources():
     return databaseobject.getresources()
 
-@app.route('/config/database/get/cartentries')
+
+@app.route("/config/database/get/cartentries")
 def getcartentries():
     return databaseobject.getcartentries()
 
-@app.route('/config/database/get/generalusers')
+
+@app.route("/config/database/get/generalusers")
 def getgeneralusers():
     return databaseobject.getgeneralusers()
 
-@app.route('/config/database/get/routeassignments')
+
+@app.route("/config/database/get/routeassignments")
 def getrouteassignments():
     return databaseobject.getrouteassignments()
 
-@app.route('/config/database/get/clusters')
+
+@app.route("/config/database/get/clusters")
 def getclusters():
     return databaseobject.getclusters()
 
-@app.route('/config/database/get/nodes')
+
+@app.route("/config/database/get/nodes")
 def getnodes():
     return databaseobject.getnodes()
 
-@app.route('/config/database/get/routesteps')
+
+@app.route("/config/database/get/routesteps")
 def getroutesteps():
     return databaseobject.getroutesteps()
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4160)
+    app.run(host="0.0.0.0", port=4160)
