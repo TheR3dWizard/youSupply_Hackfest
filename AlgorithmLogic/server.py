7915e6b3bad32ea23359fde3a65591cda0978509
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-09-29 17:43:56 +0530

    typo fix

--- AlgorithmLogic/server.py
+++ AlgorithmLogic/server.py
@@ -274,7 +274,7 @@ def setdatabase():
 def setrandom():
     body = request.get_json()
     quantity, username = body["quantity"], body["username"]
-    listofnodes = randomagent.getrandomnodes(quantity, username)
+    listofnodes = randomagent.generatenodes(quantity, username)
     for node in listofnodes:
         randomid = str(uuid.uuid4())
         node_obj = Node(
