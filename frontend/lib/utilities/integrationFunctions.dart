2e2b366d7789db308003f19b35097d57ef763f6d
TheRedWizard <sreeraghavan2016@gmail.com>
2024-09-30 19:51:54 +0530

    added function to save path data

--- frontend/lib/utilities/integrationFunctions.dart
+++ frontend/lib/utilities/integrationFunctions.dart
@@ -1,6 +1,11 @@
 import "dart:convert";
+import "dart:io";
+import "package:flutter/services.dart";
 import "package:frontend/utilities/apiFunctions.dart";
+import "package:geolocator/geolocator.dart";
 import "package:http/http.dart" as http;
+import "package:frontend/utilities/fileFunctions.dart";
+import "package:path_provider/path_provider.dart";
 
 String baseUrl = 'https://algorithm.akashshanmugaraj.com';
 
@@ -9,7 +14,7 @@ Future<bool> login(String username, String password, String role) async {
   var url = Uri.parse('$baseUrl/user/login');
   var response = await http.post(url,
       body: json.encode(
-          {'username': username, 'password': password}),
+          {'username': username, 'password': password,"role": role}),
       headers: {"Content-Type": "application/json"});
   print(response.body);
   if (response.statusCode == 200) { 
@@ -37,7 +42,42 @@ Future<bool> register(String username, String password, String phone,
   return response.statusCode == 200;
 }
 
+Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+
+  return directory.path;
+}
+
+Future<File> get _localFile async {
+  final path = await _localPath;
+
+  File file = File('$path/account.json');
+  if (!file.existsSync()) {
+    await file.create(exclusive: false);
+    String jsonString = await rootBundle.loadString('assets/account.json');
+    await file.writeAsString(jsonString);
+  }
+
+  return file;
+}
+
+
 
 Future<void> getPathData() async {
+  final file = await _localFile;
+  var url = Uri.parse('$baseUrl/path/get');
+
+
+  var response = await http.post(url,body: json.encode({
+    "username": getUsername(),
+  }), headers: {
+    "Content-Type": "application/json"
+  });
+
+  if (response.statusCode == 200) {
+    var body = jsonDecode(response.body);
+    Map<String,dynamic> jsonFile = jsonDecode(await file.readAsString());
+    jsonFile['curpaths'] = body;
+  }
 
 }
